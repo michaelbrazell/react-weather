f9db0ed6a7c638be5f1b563b10d326fb475030e9
Mike Murray <michaelbrazell@gmail.com>
2016-10-05 17:27:53 -0400

    Add in loading state to app

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -6,8 +6,7 @@ var openWeatherMap = require('openWeatherMap');
 var Weather = React.createClass({
   getInitialState: function () {
     return {
-      location: 'Boston',
-      temp: '32'
+      isLoading: false
     }
   },
   handleSearch: function (location) {
@@ -18,22 +17,38 @@ var Weather = React.createClass({
       so that 'this' inside the function returns this.setState (weather -> handleSearch)
     */
     var that = this;
+
+    this.setState({isLoading: true});
+
     openWeatherMap.getTemp(location).then(function (temp) {
       that.setState({
         location: location,
-        temp: temp
+        temp: temp,
+        isLoading: false
       });
     }, function (errorMessage) {
       alert(errorMessage);
+      that.setState({
+        isLoading: false
+      });
     });
   },
   render: function () {
-    var {temp, location} = this.state; // Uses ES6 destructuring. Equal to this.state.temp or this.state.location;
+    var {temp, location, isLoading} = this.state; // Uses ES6 destructuring. Equal to this.state.temp or this.state.location;
+
+    function renderMessage() {
+      if (isLoading) { // if isLoading is true
+        return <h3>Fetching weather...</h3>;
+      } else if (temp && location) { // if temp and location exist
+        return <WeatherMessage temp={temp} location={location}/>;
+      }
+    }
+
     return (
       <div>
         <h3>Weather Component</h3>
         <WeatherForm onSearch={this.handleSearch}/>
-        <WeatherMessage temp={temp} location={location}/>
+        {renderMessage()}
       </div>
 
     );
