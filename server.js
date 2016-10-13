623cb482734da116168ec59790d0aa1e4326f5e1
Mike Murray <michaelbrazell@gmail.com>
2016-10-13 14:55:39 -0400

    Add in Express middleware, prepping for Heroku

--- server.js
+++ server.js
@@ -3,11 +3,24 @@ var express = require('express');
 
 // Create our app
 var app = express();
+const PORT = process.env.PORT  || 3000;
+
+/*
+  OpenWeatherMap doesnt' work on HTTPS so we have to Redirect HTTP traffic
+  This is express middleware, expecting a request, response, and next
+*/
+app.use(function (req, res, next) {
+  if (req.headers['x-forwarded-proto'] === 'http') {
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+})
 
 // Call which folder to use, express starts static server, serving the directory "public"
 app.use(express.static('public'));
 
 // Runs on port 3000
-app.listen(3000, function() {
-  console.log('Express server running on port 3000');
+app.listen(PORT, function() {
+  console.log('Express server running on port ' + PORT);
 });
